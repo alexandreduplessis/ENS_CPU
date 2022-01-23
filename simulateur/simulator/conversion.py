990019054262baed3cce85393dab5f64217578d4
alexandreduplessis <94741069+alexandreduplessis@users.noreply.github.com>
2022-01-23 13:11:31 +0100

    Add files via upload

---
+++ simulateur/simulator/conversion.py
@@ -0,0 +1,11 @@
+total = int(input())
+f = open("rom2", 'r')
+chaine = ""
+for line in f:
+   chaine += line[:-2]
+f.close()
+l = len(chaine)
+zero = total-l
+f = open("rom", 'w')
+f.write(chaine+"0"*zero)
+f.close()
