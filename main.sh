0a92aca4b464ec6e72f71f5faea202e8af49c2a7
alexandreduplessis <94741069+alexandreduplessis@users.noreply.github.com>
2022-01-24 19:33:52 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 cd assembleur
+duune clean
 dune build assembleur.exe
 dune exec ./assembleur.exe fichier.s
-cp ./rom ../simulateur/rom
+cp rom ../simulateur/rom
 cd ../simulateur
 make clean
 make
 ./compiler.native ../processeur/test.net
 cp ../processeur/test ../gui/a.out
-cp ./rom ../gui/rom
+cp rom ../gui/rom
 cd ../gui
 python3 test.py
